b49ee4897e850382570ffd7e7ae7305be4d85009
isanlara <isadoranovy@gmail.com>
2023-11-18 15:59:53 -0300

    Navegação nas páginas Home e Histórico

---
+++ codigo-fonte/Histórico/scriptsHistorico.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.bt').addEventListener('click', function() {
+    var novaURL = 'https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2023-2-e1-proj-web-t7-lista-de-compras/codigo-fonte/Home/indexHome.html'; 
+    window.location.href = novaURL;
+});
+});
+
+        
---
+++ codigo-fonte/Home/scriptsHome.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.bt-nav').addEventListener('click', function() {
+    var novaURL = 'https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2023-2-e1-proj-web-t7-lista-de-compras/codigo-fonte/Hist%C3%B3rico/indexHistorico.html'; 
+    window.location.href = novaURL;
+});
+});
+
+        
