f8e8c5c94e8bf9422527585ad8edc57dc2d5484b
isanlara <isadoranovy@gmail.com>
2023-11-21 19:38:06 -0300

    atualização na funcionalidade de navegação

--- codigo-fonte/Histórico/scriptsHistorico.js
+++ codigo-fonte/Histórico/scriptsHistorico.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.bt').addEventListener('click', function() {
+const buttonNav=document.getElementById('nav');
+
+buttonNav.addEventListener('click', function() {
     var novaURL = 'https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2023-2-e1-proj-web-t7-lista-de-compras/codigo-fonte/Home/indexHome.html'; 
     window.location.href = novaURL;
 });
-});
-
-        
--- codigo-fonte/Home/scriptsHome.js
+++ codigo-fonte/Home/scriptsHome.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.bt-nav').addEventListener('click', function() {
+const buttonNav=document.getElementById('nav');
+
+buttonNav.addEventListener('click', function() {
     var novaURL = 'https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2023-2-e1-proj-web-t7-lista-de-compras/codigo-fonte/Hist%C3%B3rico/indexHistorico.html'; 
     window.location.href = novaURL;
 });
-});
-
         
