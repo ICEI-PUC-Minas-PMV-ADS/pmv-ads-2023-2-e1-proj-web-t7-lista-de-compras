272ddd59fb57d9587398cb83e8342f68c2f99c48
SamuelFilipe04 <SamuelFilipe109@gmail.com>
2023-11-22 20:31:02 -0300

    funcionalidade de criar listas iniciadas

--- codigo-fonte/Home/scriptsHome.js
+++ codigo-fonte/Home/scriptsHome.js
@@ -1,7 +1,11 @@
 const buttonNav=document.getElementById('nav');
+const formulario=document.getElementById('formulario');
 
 buttonNav.addEventListener('click', function() {
     var novaURL = 'https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2023-2-e1-proj-web-t7-lista-de-compras/codigo-fonte/Hist%C3%B3rico/indexHistorico.html'; 
     window.location.href = novaURL;
 });
+function newlist () {
+  formulario.style.display='flex' 
+}
         
