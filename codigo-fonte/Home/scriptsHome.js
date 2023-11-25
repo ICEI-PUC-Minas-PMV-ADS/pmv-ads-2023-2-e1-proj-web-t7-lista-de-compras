938bc522f0f0d6e9c56c4eaa2391d4d6aa440a06
PedroNogueir4 <pedroffn209@gmail.com>
2023-11-25 15:02:26 -0300

    funcionalidade de gerenciamentos de listas,feat:Samuel

--- codigo-fonte/Home/scriptsHome.js
+++ codigo-fonte/Home/scriptsHome.js
@@ -30,3 +30,30 @@ function saveLists(listOp) {
     list.push(listOp) 
     localStorage.setItem('lists', JSON.stringify(list)) 
 }
+
+document.addEventListener('click', function (event) { 
+  if (event.target.classList.contains('bt-delete') || event.target.closest('.bt-delete')) { 
+      let button = event.target.closest('.bt-delete');
+      let parentDiv = button.parentNode; 
+      let listName = parentDiv.querySelector('.nameL').textContent; 
+     
+
+      let lists = JSON.parse(localStorage.getItem('lists')) || []; 
+      let listD = lists.find(L => L.nome === listName)  
+
+      if (listD) { 
+          lists = lists.filter(L => L !== listD)  
+
+      }
+
+      localStorage.setItem('lists', JSON.stringify(lists)) 
+      location.reload() 
+  }
+})
+
+document.addEventListener('click',function (event) {
+  if(event.target.classList.contains('list')){
+    const id = event.target.children[1].innerHTML
+    window.location.href = `https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2023-2-e1-proj-web-t7-lista-de-compras/codigo-fonte/Listas/indexListas.html?id=${id}`
+  }
+})
--- codigo-fonte/Listas/scripts.js
+++ codigo-fonte/Listas/scripts.js
@@ -1,6 +1,11 @@
 const addButton = document.getElementById('add-bt')
-let formulario = document.getElementById("formulario") // pega o elemento com class definido
+let formulario = document.getElementById("formulario")
 
+let currentId 
+document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    currentId = params.get('id');
+})
 
 function newProduct() { //função para chamar o formulário de adicionar produto 
     formulario.style.display = "block" // chama a variavel que contem o elemento HTML e muda o display do elemento para block,fazendo-o aparecer na tela
@@ -9,13 +14,6 @@ function newProduct() { //função para chamar o formulário de adicionar produt
 function closeForm() {
     formulario.style.display = "none"  // chama a variavel que contem o elemento HTML e muda o display do elemento para none,fazendo-o desaparecer na tela
 }
-function saveProduct(product) { //função para salvar produto no localStorage,recebe como paramentro o produto a ser salvo
-    let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
-    produtos.push(product) // .push pega o produto que chega como parametro e joga dentro do array
-    localStorage.setItem('produtos', JSON.stringify(produtos)) //salva no localStorage
-
-}
-
 function validationForm() { //valida os dados que o usuário envia no formulario
 
     let name = document.getElementById("name").value             
@@ -39,10 +37,15 @@ function validationForm() { //valida os dados que o usuário envia no formulario
         alert("Digite a quantidade!")
         return false
     }
-    saveProduct({ name, price, category,qntd}) //se passar em todas as validações ele executa a função de salvar e passa os dados que vao ser salvas dentro de um objeto
+    saveProduct({name, price, category,qntd}) //se passar em todas as validações ele executa a função de salvar e passa os dados que vao ser salvas dentro de um objeto
     return true
 }
 
+function saveProduct(product) { //função para salvar produto no localStorage,recebe como paramentro o produto a ser salvo
+    let produtos = JSON.parse(localStorage.getItem(`produtos${currentId}`)) || [];
+    produtos.push(product) // .push pega o produto que chega como parametro e joga dentro do array
+    localStorage.setItem(`produtos${currentId}`, JSON.stringify(produtos)) //salva no localStorage
+}
 
 document.addEventListener('click', function (event) {  //captação de cliquer na tela e função para deletar produto da lista
     if (event.target.classList.contains('bt-delete') || event.target.closest('.bt-delete')) {  //ao clicar ele retorna tudo sobre oq foi clicado,se no que foi clicado tiver a class 'bt-delete' ele executa o resto
@@ -51,7 +54,7 @@ document.addEventListener('click', function (event) {  //captação de cliquer n
         let productName = parentDiv.querySelector('.nameP').textContent; // pega o nome do produto em que o button de deletar foi clicado
        
 
-        let produtos = JSON.parse(localStorage.getItem('produtos')) || []; // busca os produtos salvos no localStorage
+        let produtos = JSON.parse(localStorage.getItem(`produtos${currentId}`)) || []; // busca os produtos salvos no localStorage
         let productD = produtos.find(p => p.name === productName)  // encontra dentro do array de produtos o produto que tem o mesmo nome do que o button foi clicado
 
         if (productD) { // se ele encontrar o produto ou seja se a variavel productD for true ele executa
@@ -59,7 +62,7 @@ document.addEventListener('click', function (event) {  //captação de cliquer n
 // percorre o array e filtra por meio do nome e retorna todos os elementos que não tem aquele nome e dps salva na variavel
         }
 
-        localStorage.setItem('produtos', JSON.stringify(produtos)) // salva no localStorage sem o produto que foi deletado
+        localStorage.setItem(`produtos${currentId}`,JSON.stringify(produtos)) // salva no localStorage sem o produto que foi deletado
         location.reload() //recarrega a página automaticamente para atualizar a lista
     }
 })
